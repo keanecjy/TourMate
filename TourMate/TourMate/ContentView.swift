0820780fddbaca9ae896a190ece202bc29418090
tanruiquan <ruiquann@gmail.com>
2022-03-08 14:57:52 +0800

    Add project

---
+++ TourMate/TourMate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TourMate
+//
+//  Created by Tan Rui Quan on 8/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
