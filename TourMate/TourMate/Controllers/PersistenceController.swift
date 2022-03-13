b0a76cece882168554f53825b56217940b072806
Keane Chan <e0425601@u.nus.edu>
2022-03-13 13:46:01 +0800

    fix arm64 build issue

---
+++ TourMate/TourMate/Controllers/PersistenceController.swift
@@ -0,0 +1,29 @@
+//
+//  PersistenceController.swift
+//  TourMate
+//
+//  Created by Keane Chan on 12/3/22.
+//
+
+import FirebaseFirestore
+
+class PersistenceController {
+    // func fetchTrip(docId: String) {
+    // }
+    // 
+    // func addTrip(trip: Trip) {
+    //     
+    // }
+    // 
+    // func updateTrip(docId: String, trip: Trip) {
+    //     
+    // }
+    // 
+    // func deleteTrip(docId: String, trip: Trip) {
+    //     
+    // }
+    // 
+    // func deleteTrip(docId: String, trip: Trip) {
+    //     
+    // }
+}
