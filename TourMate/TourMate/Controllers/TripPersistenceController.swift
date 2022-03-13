aa3b53cd3706e8cc8a575013fca12229b73a244e
Keane Chan <e0425601@u.nus.edu>
2022-03-14 01:47:48 +0800

    add new persistence files

---
+++ TourMate/TourMate/Controllers/TripPersistenceController.swift
@@ -0,0 +1,8 @@
+//
+//  TripPersistenceController.swift
+//  TourMate
+//
+//  Created by Keane Chan on 13/3/22.
+//
+
+import Foundation
---
+++ TourMate/TourMate/Models/NewTrip.swift
@@ -0,0 +1,8 @@
+//
+//  NewTrip.swift
+//  TourMate
+//
+//  Created by Keane Chan on 14/3/22.
+//
+
+import Foundation
