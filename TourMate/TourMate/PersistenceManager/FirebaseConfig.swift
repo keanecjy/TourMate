a9298b22d43c1fff2255d2e358b23b2ddbb9eaa9
Keane Chan <e0425601@u.nus.edu>
2022-03-13 17:38:21 +0800

    add config keys for firebase

---
+++ TourMate/TourMate/PersistenceManager/FirebaseConfig.swift
@@ -0,0 +1,13 @@
+//
+//  FirebaseCollectionIds.swift
+//  TourMate
+//
+//  Created by Keane Chan on 13/3/22.
+//
+
+final class FirebaseConfig {
+    static let accountCollectionId = "account"
+    static let tripCollectionId = "trips"
+    static let planCollectionId = "plans"
+    static let planDetailsCollectionId = "plandetails"
+}
