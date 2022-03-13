39b3af39e0ce4a34bb1a354069c287ea2632058b
Keane Chan <e0425601@u.nus.edu>
2022-03-13 18:28:28 +0800

    Use generic PersistenceController.swift

---
+++ TourMate/TourMate/PersistenceManager/FirebasePersistenceManager.swift
@@ -0,0 +1,69 @@
+//
+//  FirebasePersistenceManager.swift
+//  TourMate
+//
+//  Created by Keane Chan on 13/3/22.
+//
+
+import Firebase
+
+struct FirebasePersistenceManager<T: Codable>: PersistenceManager {
+    var collectionId: String
+    
+    private let db = Firestore.firestore()
+    
+    @MainActor
+    func fetchItems(field: String, id: String) async -> (trips: [T], errorMessage: String) {
+        var items: [T] = []
+        var errorMessage = ""
+        
+        do {
+            let query = db.collection(collectionId).whereField(field, arrayContains: id)
+            let documents = try await query.getDocuments().documents
+            items = documents.compactMap({ try? $0.data(as: T.self) })
+        } catch {
+            errorMessage = "Error fetching \(T.self): \(error)"
+        }
+        
+        return (items, errorMessage)
+    }
+    
+    @MainActor
+    func addItem(item: T) async -> (hasAddedItem: Bool, errorMessage: String) {
+        var hasAddedItem = false
+        var errorMessage = ""
+        
+        do {
+            let newItemRef = try db.collection(collectionId).addDocument(from: item)
+            hasAddedItem = true
+            print("[FirebasePersistenceManager] Added \(T.self): \(newItemRef)")
+        } catch {
+            errorMessage = "[FirebasePersistenceManager] Error adding \(T.self): \(error)"
+        }
+        
+        return (hasAddedItem, errorMessage)
+    }
+    
+    @MainActor
+    func deleteItem(id: String) async -> (hasDeletedItem: Bool, errorMessage: String) {
+        var hasDeletedItem = false
+        var errorMessage: String = ""
+        
+        do {
+            let deletedItemRef = db.collection(collectionId).document(id)
+            try await deletedItemRef.delete()
+            hasDeletedItem = true
+            print("[FirebasePersistenceManager] Deleted \(T.self): \(deletedItemRef)")
+        } catch {
+            errorMessage = "[FirebasePersistenceManager] Error deleting \(T.self): \(error)"
+        }
+        
+        return (hasDeletedItem, errorMessage)
+    }
+    
+    @MainActor
+    func updateItem(item: T) async -> (hasUpdatedItem: Bool, errorMessage: String) {
+        let (hasAddedItem, errorMessage) = await addItem(item: item)
+        return (hasAddedItem, errorMessage)
+    }
+}
---
+++ TourMate/TourMate/PersistenceManager/PersistenceManager.swift
@@ -0,0 +1,15 @@
+//
+//  PersistenceManager.swift
+//  TourMate
+//
+//  Created by Keane Chan on 13/3/22.
+//
+
+protocol PersistenceManager {
+    associatedtype T: Codable
+    
+    func fetchItems(field: String, id: String) async -> (trips: [T], errorMessage: String)
+    func addItem(item: T) async -> (hasAddedItem: Bool, errorMessage: String)
+    func deleteItem(id: String) async -> (hasDeletedItem: Bool, errorMessage: String)
+    func updateItem(item: T) async -> (hasUpdatedItem: Bool, errorMessage: String)
+}
