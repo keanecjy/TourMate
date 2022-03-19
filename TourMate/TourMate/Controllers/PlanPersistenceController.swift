82a7088bf9dc7ea19b7ab5ed7a9613367bd8d84a
Keane Chan <e0425601@u.nus.edu>
2022-03-19 23:26:45 +0800

    swiftlint

--- TourMate/TourMate/Controllers/PlanPersistenceController.swift
+++ TourMate/TourMate/Controllers/PlanPersistenceController.swift
@@ -8,19 +8,19 @@
 import FirebaseAuth
 
 struct PlanPersistenceController: PlanPersistenceControllerProtocol {
-    
+
     let firebasePersistenceManager = FirebasePersistenceManager(collectionId: FirebaseConfig.planCollectionId)
 
     func addPlan(plan: Plan) async -> (Bool, String) {
         await firebasePersistenceManager.addItem(id: plan.id, item: PlanAdapter.toAdaptedPlan(plan: plan))
     }
-    
+
     func fetchPlan(withPlanId planId: String) async -> (Plan, String) {
         let (adaptedPlan, errorMessage) = await firebasePersistenceManager.fetchItem(id: planId)
         guard let adaptedPlan = adaptedPlan as? FirebaseAdaptedPlan else {
             preconditionFailure()
         }
-        
+
         let plan = PlanAdapter.toPlan(adaptedPlan: adaptedPlan)
         return (plan, errorMessage)
     }
