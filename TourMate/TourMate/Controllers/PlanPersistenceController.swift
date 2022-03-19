dad0302260df4a8e00d14e386ca11a6a1141bd1b
Rayner Lim <rlrh1996@gmail.com>
2022-03-19 15:55:31 +0800

    fix UI

--- TourMate/TourMate/Controllers/PlanPersistenceController.swift
+++ TourMate/TourMate/Controllers/PlanPersistenceController.swift
@@ -7,9 +7,4 @@
 
 struct PlanPersistenceController {
     // TODO: CRUD for plans - To think of better way to generalize things
-    
-    func addPlan<T: Plan>(plan: T) async -> (Bool, String) {
-        await firebasePersistenceManager.addItem(id: plan.id, item: plan.toData())
-    }
-
 }
