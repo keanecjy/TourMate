6fd5250868612f51ca60cdfb7880647dfa3b93c8
Terence Ho <>
2022-03-26 12:25:59 +0800

    Add method to upvote

--- TourMate/TourMate/Frontend/Plan/Views/PlanViews/PlanViewModel.swift
+++ TourMate/TourMate/Frontend/Plan/Views/PlanViews/PlanViewModel.swift
@@ -14,17 +14,23 @@ class PlanViewModel<T: Plan>: ObservableObject {
     @Published private(set) var hasError: Bool
 
     @Published private(set) var userHasUpvotedPlan: Bool
+    @Published private(set) var upvotedUsers: [User]
 
     let planController: PlanController
+    let userController: UserController
     var planId: String
 
-    init(planController: PlanController = FirebasePlanController(), planId: String) {
+    init(planController: PlanController = FirebasePlanController(),
+         userController: UserController = FirebaseUserController(),
+         planId: String) {
         self.isLoading = false
         self.hasError = false
         self.planController = planController
+        self.userController = userController
         self.planId = planId
 
         self.userHasUpvotedPlan = false
+        self.upvotedUsers = []
     }
 
     func fetchPlan() async {
@@ -57,6 +63,50 @@ class PlanViewModel<T: Plan>: ObservableObject {
     }
 
     func upvotePlan() async {
+        self.isLoading = true
+
+        let (user, userErrorMessage) = await userController.getUser()
+
+        guard let user = user, userErrorMessage.isEmpty else {
+            self.isLoading = false
+            self.hasError = true
+            return
+        }
+
+        let userId = user.id
+
+        // there's no plan -> not error
+        guard let updatedPlan = updateUpvotes(id: userId) else {
+            self.isLoading = false
+            return
+        }
+
+        let (hasUpdatedPlan, planErrorMessage) = await planController.updatePlan(plan: updatedPlan)
+
+        guard hasUpdatedPlan, planErrorMessage.isEmpty else {
+            self.isLoading = false
+            self.hasError = true
+            return
+        }
+
+        self.plan = updatedPlan
         self.userHasUpvotedPlan.toggle()
+
+        self.isLoading = false
+    }
+
+    private func updateUpvotes(id: String) -> T? {
+        guard var plan = plan else {
+            return nil
+        }
+
+        if plan.upvotedUserIds.contains(id) {
+            plan.upvotedUserIds = plan.upvotedUserIds.filter { $0 != id }
+        } else {
+            plan.upvotedUserIds.append(id)
+        }
+
+        return plan
+
     }
 }
