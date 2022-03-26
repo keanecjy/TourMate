8b18a3a08d3e27279cd64438d7ec762561da91c5
Terence Ho <>
2022-03-26 17:02:23 +0800

    Update fields in comments

--- TourMate/TourMate/Frontend/Plan/Models/Components/Comment.swift
+++ TourMate/TourMate/Frontend/Plan/Models/Components/Comment.swift
@@ -5,9 +5,13 @@
 //  Created by Keane Chan on 14/3/22.
 //
 
+import Foundation
+
 struct Comment {
+    let planId: String
+    let id: String
     let name: String
     let message: String
-    let upVote: Int
-    let downVote: Int
+    let creationDate: Date
+    var upvotedUserIds: [String]
 }
