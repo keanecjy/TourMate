e6b4a620c3aa6a988d62ab90d8323057d5dfb98f
tanruiquan <ruiquann@gmail.com>
2022-03-31 17:45:00 +0800

    Shift components to their own file

--- TourMate/TourMate/Backend/Network/Models/APICall.swift
+++ TourMate/TourMate/Backend/Network/Models/APICall.swift
@@ -14,28 +14,6 @@ protocol APICall {
     func body() -> Data?
 }
 
-enum APIError: Swift.Error {
-    case invalidURL
-    case httpCode(HTTPCode)
-    case unexpectedResponse
-    case imageProcessing([URLRequest])
-}
-
-extension APIError: LocalizedError {
-    var errorDescription: String? {
-        switch self {
-        case .invalidURL:
-            return "Invalid URL"
-        case let .httpCode(code):
-            return "Unexpected HTTP code: \(code)"
-        case .unexpectedResponse:
-            return "Unexpected response from the server"
-        case .imageProcessing:
-            return "Unable to load image"
-        }
-    }
-}
-
 extension APICall {
     func urlRequest(baseURL: String) throws -> URLRequest {
         guard let url = URL(string: baseURL + path) else {
--- TourMate/TourMate/Frontend/Plan/Services/LocationService.swift
+++ TourMate/TourMate/Frontend/Plan/Services/LocationService.swift
@@ -10,25 +10,3 @@ import Foundation
 protocol LocationService {
     func fetchLocations(query: String) async -> ([Location], String)
 }
-
-struct RealLocationService: LocationService {
-    private let locationWebRepository = RealLocationWebRepository(baseURL: ApiConfig.geoapifyBaseUrl)
-
-    private let locationAdapter = LocationAdapter()
-
-    func fetchLocations(query: String) async -> ([Location], String) {
-        print("[LocationService] Fetching Locations")
-
-        let autocompleteQuery = AutocompleteQuery(apiKey: ApiKeys.geopifyApiKey, text: query)
-
-        let (adaptedLocations, errorMessage) = await locationWebRepository.fetchLocations(query: autocompleteQuery)
-
-        guard errorMessage.isEmpty else {
-            return ([], errorMessage)
-        }
-
-        let locations = adaptedLocations
-            .map({ locationAdapter.toLocation(adaptedLocation: $0) })
-        return (locations, "")
-    }
-}
