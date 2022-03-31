d8133f56deddff502b58fbf0e380a0ee1cc2d956
tanruiquan <ruiquann@gmail.com>
2022-03-31 14:11:46 +0800

    Refactor Location

--- TourMate/TourMate/Frontend/Plan/Services/MockLocationService.swift
+++ TourMate/TourMate/Frontend/Plan/Services/MockLocationService.swift
@@ -9,18 +9,26 @@ import Foundation
 
 class MockLocationService: LocationService {
     let locations: [Location] = [
-        Location(addressShort: "SCA",
-                 addressLong: "Singapore Changi Airport",
-                 addressFull: "SCA, Singapore Changi Airport",
-                 longitude: 1.2, latitude: 3.4),
-        Location(addressShort: "NUS",
-                 addressLong: "National University of Singapore",
+        Location(addressLineOne: "NUS",
+                 addressLineTwo: "National University of Singapore",
                  addressFull: "NUS, National University of Singapore",
-                 longitude: 4.5, latitude: 6.7),
-        Location(addressShort: "NTU",
-                 addressLong: "Nanyang Technological University",
+                 longitude: 1.2966, latitude: 103.7764),
+        Location(addressLineOne: "NTU",
+                 addressLineTwo: "Nanyang Technological University",
                  addressFull: "NTU, Nanyang Technological University",
-                 longitude: 1.9, latitude: 4.5)
+                 longitude: 1.3483, latitude: 103.6831),
+        Location(addressLineOne: "SMU",
+                 addressLineTwo: "Singapore Management University",
+                 addressFull: "SMU, Singapore Management University",
+                 longitude: 1.2963, latitude: 103.8502),
+        Location(addressLineOne: "SUTD",
+                 addressLineTwo: "Singapore University of Technology and Design",
+                 addressFull: "SUTD, Singapore University of Technology and Design",
+                 longitude: 1.3414, latitude: 103.9633),
+        Location(addressLineOne: "SCA",
+                 addressLineTwo: "Singapore Changi Airport",
+                 addressFull: "SCA, Singapore Changi Airport",
+                 longitude: 1.3644, latitude: 103.9915)
     ]
 
     func fetchLocations(query: String) async -> ([Location], String) {
