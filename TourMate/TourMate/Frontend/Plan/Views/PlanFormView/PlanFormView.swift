62ddf35e993493e350c93204d0bb3bc782a4dae4
Terence Ho <>
2022-04-01 17:30:47 +0800

    Add PlanFormVM

--- TourMate/TourMate/Frontend/Plan/Views/PlanFormView/PlanFormView.swift
+++ TourMate/TourMate/Frontend/Plan/Views/PlanFormView/PlanFormView.swift
@@ -8,13 +8,50 @@
 import SwiftUI
 
 struct PlanFormView: View {
+
+    @State var isShowingSearchStartLocSheet: Bool = false
+    @State var isShowingSearchEndLocSheet: Bool = false
+    @ObservedObject var viewModel: PlanFormViewModel
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            ConfirmedToggle(status: $viewModel.planStatus)
+
+            TextField("Event Name", text: $viewModel.planName)
+
+            DatePicker("Start Date",
+                       selection: $viewModel.planStartDate,
+                       in: viewModel.lowerBoundDate...viewModel.upperBoundDate,
+                       displayedComponents: [.date, .hourAndMinute])
+
+            DatePicker("End Date",
+                       selection: $viewModel.planEndDate,
+                       in: viewModel.lowerBoundDate...viewModel.upperBoundDate,
+                       displayedComponents: [.date, .hourAndMinute])
+
+            TextField("Start Location", text: $viewModel.planStartLocation)
+                .sheet(isPresented: $isShowingSearchStartLocSheet) {
+                    SearchView(viewModel: SearchViewModel(), planAddress: $viewModel.planStartLocation)
+                }
+                .onTapGesture {
+                    isShowingSearchStartLocSheet.toggle()
+                }
+
+            TextField("End Location", text: $viewModel.planEndLocation)
+                .sheet(isPresented: $isShowingSearchEndLocSheet) {
+                    SearchView(viewModel: SearchViewModel(), planAddress: $viewModel.planEndLocation)
+                }
+                .onTapGesture {
+                    isShowingSearchEndLocSheet.toggle()
+                }
+
+            // TODO: Add Additional Info box
+        }
     }
 }
 
-struct PlanFormView_Previews: PreviewProvider {
-    static var previews: some View {
-        PlanFormView()
-    }
-}
+// struct PlanFormView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        PlanFormView()
+//    }
+// }
---
+++ TourMate/TourMate/Frontend/Plan/Views/PlanFormView/PlanFormViewModel.swift
@@ -0,0 +1,76 @@
+//
+//  PlanFormViewModel.swift
+//  TourMate
+//
+//  Created by Terence Ho on 1/4/22.
+//
+
+import Foundation
+import Combine
+
+@MainActor
+class PlanFormViewModel: ObservableObject {
+    let lowerBoundDate: Date
+    let upperBoundDate: Date
+
+    @Published var isPlanNameValid: Bool
+    @Published var isPlanDurationValid: Bool
+    @Published var canSubmitPlan: Bool
+
+    @Published var planStatus: PlanStatus
+    @Published var planName: String
+    @Published var planStartDate: Date
+    @Published var planEndDate: Date
+    @Published var planStartLocation: String
+    @Published var planEndLocation: String
+    @Published var planAdditionalInfo: String
+
+    private var cancellableSet: Set<AnyCancellable> = []
+
+    init(lowerBoundDate: Date, upperBoundDate: Date) {
+        self.lowerBoundDate = lowerBoundDate
+        self.upperBoundDate = upperBoundDate
+
+        self.isPlanNameValid = false
+        self.isPlanDurationValid = true
+        self.canSubmitPlan = false
+
+        self.planStatus = .proposed
+        self.planName = ""
+        self.planStartDate = lowerBoundDate
+        self.planEndDate = lowerBoundDate
+        self.planStartLocation = ""
+        self.planEndLocation = ""
+        self.planAdditionalInfo = ""
+
+        // Plan Constraints
+        $planName
+            .map({ !$0.isEmpty })
+            .assign(to: \.isPlanNameValid, on: self)
+            .store(in: &cancellableSet)
+
+        Publishers
+            .CombineLatest($planStartDate, $planEndDate)
+            .map({ max($0, $1) })
+            .assign(to: \.planEndDate, on: self)
+            .store(in: &cancellableSet)
+
+        // Within date boundaries
+        Publishers
+            .CombineLatest($planStartDate, $planEndDate)
+            .map({ start, end in
+                lowerBoundDate <= start &&
+                start <= end &&
+                end <= upperBoundDate
+            })
+            .assign(to: \.isPlanDurationValid, on: self)
+            .store(in: &cancellableSet)
+
+        // All validity checks satisfied
+        Publishers
+            .CombineLatest($isPlanNameValid, $isPlanDurationValid)
+            .map({ $0 && $1 })
+            .assign(to: \.canSubmitPlan, on: self)
+            .store(in: &cancellableSet)
+    }
+}
