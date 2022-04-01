c35e9e353ee582801428601ae8d563c3dbd4fa21
tanruiquan <ruiquann@gmail.com>
2022-04-01 20:24:31 +0800

    Add TimingView

---
+++ TourMate/TourMate/Frontend/Plan/Views/PlanViews/Components/TimingView.swift
@@ -0,0 +1,38 @@
+//
+//  TimingView.swift
+//  TourMate
+//
+//  Created by Tan Rui Quan on 1/4/22.
+//
+
+import SwiftUI
+
+struct TimingView: View {
+    @Binding var plan: Plan
+    let dateFormatter: DateFormatter
+
+    init(plan: Binding<Plan>) {
+        _plan = plan
+        dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .short
+    }
+    var body: some View {
+        HStack(alignment: .top) {
+            Image(systemName: "clock")
+            VStack(alignment: .leading) {
+                Text("From").font(.caption)
+                Text(plan.startDateTime.date, formatter: dateFormatter)
+                Text("To").font(.caption)
+                Text(plan.endDateTime.date, formatter: dateFormatter)
+            }
+        }
+    }
+}
+
+struct TimingView_Previews: PreviewProvider {
+    static var previews: some View {
+        @ObservedObject var planViewModel = PlanViewModel(plan: MockPlanService().plans[0], trip: MockTripService().trips[0])
+        return TimingView(plan: $planViewModel.plan)
+    }
+}
