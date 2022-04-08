0dfd09bf243bc1b473bee5fb87b2b828eeee6000
Keane Chan <e0425601@u.nus.edu>
2022-04-08 17:43:40 +0800

    add repositories to app config

--- TourMate/TourMate/Backend/Config/AppConfig.swift
+++ TourMate/TourMate/Backend/Config/AppConfig.swift
@@ -29,6 +29,30 @@ private struct LocationServiceKey: InjectionKey {
     static var currentValue: LocationService = RealLocationService()
 }
 
+private struct UserRepositoryKey: InjectionKey {
+    static var currentValue: Repository = FirebaseRepository(collectionId: FirebaseConfig.userCollectionId)
+}
+
+private struct TripRepositoryKey: InjectionKey {
+    static var currentValue: Repository = FirebaseRepository(collectionId: FirebaseConfig.tripCollectionId)
+}
+
+private struct PlanRepositoryKey: InjectionKey {
+    static var currentValue: Repository = FirebaseRepository(collectionId: FirebaseConfig.planCollectionId)
+}
+
+private struct CommentRepositoryKey: InjectionKey {
+    static var currentValue: Repository = FirebaseRepository(collectionId: FirebaseConfig.commentCollectionId)
+}
+
+private struct PlanUpvoteRepositoryKey: InjectionKey {
+    static var currentValue: Repository = FirebaseRepository(collectionId: FirebaseConfig.upvoteCollectionId)
+}
+
+private struct LocationWebRepositoryKey: InjectionKey {
+    static var currentValue: LocationWebRepository = RealLocationWebRepository(baseURL: ApiConfig.geoapifyBaseUrl)
+}
+
 extension InjectedValues {
     var userService: UserService {
         get { Self[UserServiceKey.self] }
@@ -58,5 +82,35 @@ extension InjectedValues {
     var locationService: LocationService {
         get { Self[LocationServiceKey.self] }
         set { Self[LocationServiceKey.self] = newValue }
+    }
+
+    var userRepository: Repository {
+        get { Self[UserRepositoryKey.self] }
+        set { Self[UserRepositoryKey.self] = newValue }
+    }
+
+    var tripRepository: Repository {
+        get { Self[TripRepositoryKey.self] }
+        set { Self[TripRepositoryKey.self] = newValue }
+    }
+
+    var planRepository: Repository {
+        get { Self[PlanRepositoryKey.self] }
+        set { Self[PlanRepositoryKey.self] = newValue }
+    }
+
+    var commentRepository: Repository {
+        get { Self[CommentRepositoryKey.self] }
+        set { Self[CommentRepositoryKey.self] = newValue }
+    }
+
+    var planUpvoteRepository: Repository {
+        get { Self[PlanUpvoteRepositoryKey.self] }
+        set { Self[PlanUpvoteRepositoryKey.self] = newValue }
+    }
+
+    var locationWebRepository: LocationWebRepository {
+        get { Self[LocationWebRepositoryKey.self] }
+        set { Self[LocationWebRepositoryKey.self] = newValue }
     }
 }
