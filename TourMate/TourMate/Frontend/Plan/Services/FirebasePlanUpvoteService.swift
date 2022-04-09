b93daf136f1789359bf247cc6d476d4240e9dce2
Keane Chan <e0425601@u.nus.edu>
2022-04-09 14:01:58 +0800

    rename to planUpvoteRepository

--- TourMate/TourMate/Frontend/Plan/Services/FirebasePlanUpvoteService.swift
+++ TourMate/TourMate/Frontend/Plan/Services/FirebasePlanUpvoteService.swift
@@ -12,7 +12,7 @@ class FirebasePlanUpvoteService: PlanUpvoteService {
 
     }
 
-    private let firebaseRepository = FirebaseRepository(collectionId: FirebaseConfig.upvoteCollectionId)
+    private let planUpvoteRepository = FirebaseRepository(collectionId: FirebaseConfig.upvoteCollectionId)
 
     private let planUpvoteAdapter = PlanUpvoteAdapter()
 
@@ -21,27 +21,27 @@ class FirebasePlanUpvoteService: PlanUpvoteService {
     func fetchPlanUpvotesAndListen(withPlanId planId: String) async {
         print("[FirebasePlanUpvoteService] Fetching and listening to plan upvotes")
 
-        firebaseRepository.eventDelegate = self
-        await firebaseRepository.fetchItemsAndListen(field: "planId", isEqualTo: planId)
+        planUpvoteRepository.eventDelegate = self
+        await planUpvoteRepository.fetchItemsAndListen(field: "planId", isEqualTo: planId)
     }
 
     func addPlanUpvote(planUpvote: PlanUpvote) async -> (Bool, String) {
-        await firebaseRepository.addItem(id: planUpvote.id,
-                                         item: planUpvoteAdapter.toAdaptedPlanUpvote(planUpvote: planUpvote))
+        await planUpvoteRepository.addItem(id: planUpvote.id,
+                                           item: planUpvoteAdapter.toAdaptedPlanUpvote(planUpvote: planUpvote))
     }
 
     func deletePlanUpvote(planUpvote: PlanUpvote) async -> (Bool, String) {
-        await firebaseRepository.deleteItem(id: planUpvote.id)
+        await planUpvoteRepository.deleteItem(id: planUpvote.id)
     }
 
     func updatePlanUpvote(planUpvote: PlanUpvote) async -> (Bool, String) {
-        await firebaseRepository.updateItem(id: planUpvote.id,
-                                            item: planUpvoteAdapter.toAdaptedPlanUpvote(planUpvote: planUpvote))
+        await planUpvoteRepository.updateItem(id: planUpvote.id,
+                                              item: planUpvoteAdapter.toAdaptedPlanUpvote(planUpvote: planUpvote))
     }
 
     func detachListener() {
-        firebaseRepository.eventDelegate = nil
-        firebaseRepository.detachListener()
+        planUpvoteRepository.eventDelegate = nil
+        planUpvoteRepository.detachListener()
     }
 }
 
