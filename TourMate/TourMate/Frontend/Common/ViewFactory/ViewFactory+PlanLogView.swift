fb554d35ade716ff1114da09a1375c0ba7ee2e3f
Keane Chan <e0425601@u.nus.edu>
2022-04-16 15:20:39 +0800

    add diff string to plan log display

--- TourMate/TourMate/Frontend/Common/ViewFactory/ViewFactory+PlanLogView.swift
+++ TourMate/TourMate/Frontend/Common/ViewFactory/ViewFactory+PlanLogView.swift
@@ -13,8 +13,22 @@ extension ViewFactory {
 
     func getPlanVersionView<T: Plan>(planDisplayViewModel: PlanDisplayViewModel<T>, plan: T) -> some View {
         let action = plan.versionNumber == 1 ? "created" : "updated"
+
         let username = planDisplayViewModel.getPlanModifier(version: plan.versionNumber)?.name ?? "someone..."
-        return PlanLogDisplayHeader(header: "Plan version \(plan.versionNumber) \(action) by \(username)")
+        let diff = getDiffFromPreviousPlan(plans: planDisplayViewModel.allVersionedPlansSortedDesc, plan: plan)
+
+        return PlanLogDisplayHeader(header: "Plan version \(plan.versionNumber) \(action) by \(username)",
+                                    subheader: diff)
+    }
+
+    private func getDiffFromPreviousPlan<T: Plan>(plans: [T], plan: T) -> String {
+        guard let previousPlan = plans.first(where: { $0.versionNumber < plan.versionNumber }) else {
+            return ""
+        }
+
+        let differenceMap = plan.diff(other: previousPlan)
+
+        return differenceMap.map({ "\($0) changed from \($1.0) to \($1.1)" }).joined(separator: "\n")
     }
 
     // Will be empty if there are no views to display
